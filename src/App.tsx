d92df40a457ebdb43a5fa9858f03b0308ae8ae5b
Petro Shevchyshen <shevchyshen@gmail.com>
2025-03-21 21:26:16 +0200

    added tailwind, react-router, zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/test" element={<p>test</p>} />
+      </Routes>
       <div>Test</div>
     </>
   );
